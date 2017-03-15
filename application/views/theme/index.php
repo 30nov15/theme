74174b7958cf842980a14994db4a8b9ae5eccd2d
30nov15 <chaiwatkkd@gmail.com>
2017-03-16 01:34:58 +0700

    first commit

---
+++ application/views/theme/index.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <title>All Theme to Modify</title>
+        <link  rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>">
+        <link  rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap-theme.min.css'); ?>">
+        <script type="text/javascript" src="<?php echo base_url('assets/js/jquery-3.1.0.js'); ?>"></script>
+        <script type="text/javascript" src="<?php echo base_url().'assets/js/bootstrap.min.js'; ?>"></script>
+        <style>
+            @font-face {
+                font-family: 'Glyphicons Halflings';
+                src: url(<?php echo base_url('fonts/glyphicons-halflings-regular.eot'); ?>);
+                src: url(<?php echo base_url('fonts/glyphicons-halflings-regular.eot?#iefix'); ?>)
+                format('embedded-opentype'), 
+                    url(<?php echo base_url('fonts/glyphicons-halflings-regular.woff'); ?>)
+                     format('woff'), 
+                    url(<?php echo base_url('fonts/glyphicons-halflings-regular.ttf'); ?>)
+                     format('truetype'), 
+                    url(<?php echo base_url('fonts/glyphicons-halflings-regular.svg#glyphicons_halflingsregular'); ?>)
+                     format('svg');
+            }
+            body {
+                background-color: #eee;
+                border-radius: 0;
+            }
+            .thumbnail {
+                border-radius: 0;
+            }
+            .div-space{
+                margin: 70px 0;
+            }
+            #div-box{
+                background-color: #fff;
+                color: #bbb;
+                border-radius: 0;
+                padding: 14px;
+                margin: 6px 6px;
+                box-shadow: 0px 10px 16px -16px black;
+            }
+            hr{
+                margin: 6px 6px;
+            }
+            #div-titlebar {
+                background-color: #FF9800;
+                margin: 0;
+                padding-right: 30px;
+                
+                color: #aaa;
+
+            }
+            #navbar-menu {
+                background-color: #fff;
+                padding-top: 0;
+                margin-top: 0;
+                margin-bottom: 20px;
+                border-radius: 0;
+                box-shadow: 0px 10px 16px -14px black;
+            }
+            .thumbnail{
+                
+                margin-left: 5px;
+                text-align: center;
+            }
+            #div-footer{
+                background-color: #333333;
+                color: #aaa;
+                margin-top: 60px;
+                margin-bottom: 0;
+                border-radius: 0;
+            }
+        </style>
+    </head>
+    <body>
+        
+           
+        <nav class="navbar navbar-reverse navbar-fluid">
+            <h1>blalalal</h1>
+        </nav>
+
+        <nav class="navbar navbar-fluid" id="navbar-menu">
+            <div class="container"> 
+                <!-- Brand and toggle get grouped for better mobile display -->
+                
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1"> 
+                      <span class="sr-only">Toggle navigation</span> 
+                      <span class="icon-bar"></span> 
+                      <span class="icon-bar"></span> 
+                      <span class="icon-bar"></span> 
+                    </button>
+                  <a class="navbar-brand hidden-md hidden-sm hidden-lg" title="Theme" href="#"><span class="glyphicon glyphicon-"Theme</a>
+                  <div class="margindash pull-right hidden-lg hidden-md hidden-sm">
+
+                    <!-- Button trigger modal --> 
+                    <a href="" class="write" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-check font24"></span></a> <a href="register.php" class="write"><span class="glyphicon glyphicon-user font24"></span></a>
+              </div>
+                </div>
+    
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="navbar-collapse-1">
+        <ul class="nav navbar-nav">
+            <li><a href="#">Home </a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact Us</a></li>
+            <li><a data-toggle="modal" data-target="#myModal" href="">ลงประกาศฟรี</a></li>
+        </ul>
+    <div class="navbar-text navbar-right hidden-xs">
+                
+        <!-- Button trigger modal --> 
+        <a href="" class="write" data-toggle="modal" data-target="#myModal">เข้าสู่ระบบ</a> <span class="write">|</span> <a href="register.php" class="write">สมัครสมาชิก</a>
+              </div>
+    </div>
+    <!-- /.navbar-collapse --> 
+    
+  </div>
+  <!-- /.container-fluid --> 
+</nav>
+        
+        
+        
+        
+        
+        
+        
+        <div class="container">
+            <!--<div class="row">
+                <div class="div-space">
+                
+                </div>
+            </div>-->
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="row">
+                        <div class="thumbnail" id="div-box">
+                            <h4>Div box</h4>
+                            <hr>
+                            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="thumbnail" id="div-box">
+                            <h4>Div box</h4>
+                            <hr>
+                            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+                            <hr>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="row">
+                        <div class="thumbnail" id="div-box">
+                            <h4>Div box</h4>
+                            <br><br><br><br>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="thumbnail" id="div-box">
+                            <h4>Div box</h4>
+                            <hr>
+                            <br><br><br><br>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="thumbnail" id="div-box">
+                            <h4>Div box</h4>
+                            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer>
+            <nav class="navbar navbar-fluid" id="div-footer">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2>footer</h2>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        
+    </body>
+    <script>
+            $('#btnprime').click(function(){
+                alert('click');
+            });
+        </script>
+</html>
